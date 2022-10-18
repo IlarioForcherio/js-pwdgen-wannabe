442b921f45a87ff231a0f77583e2aa8fd650784b
ilarioforcherio <ilarioforcherio@gmail.com>
2022-10-18 16:14:11 +0200

    start project + add main.js file

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+
+/* Generiamo una password composta da:
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+aggiungiamo alla fine un numero
+Infine scriviamo sulla pagina "la tua password insicurissima è: nomecognomecolorepreferito21"*/
+
+//Chiedi all'utente il suo nome
+//-let nome = prompt();
+//Chiedi  all'utente il suo cognome
+//--let cognome = prompt();
+//Chiedi all'utente il suo colore preferito
+//-let colore = prompt();
+//aggiungiamo alla fine un numero
+//scriviamo sulla pagina "la tua password insicurissima è: nomecognomecolorepreferito21"
+
+//nome
+let name = prompt('insert your name');
+console.log(name);
+//cognome
+let surname = prompt('insert your surname');
+console.log(surname);
+//colore
+let surname = prompt('insert your surname');
+console.log(surname);
